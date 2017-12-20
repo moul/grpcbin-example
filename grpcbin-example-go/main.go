2e9e3adbbba3740524ab6623cdb1342695f339d4
Manfred Touron <m@42.am>
2017-12-20 09:43:45 +0100

    Update example

--- grpcbin-example-go/main.go
+++ grpcbin-example-go/main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 
 	pb "github.com/moul/pb/grpcbin/go-grpc"
-	"google.golang.org/grpc"
 )
 
 func main() {
+	// dial
 	conn, _ := grpc.Dial("grpcb.in:9000", grpc.WithInsecure())
 	defer conn.Close()
+
+	// create client and context
 	client := pb.NewGRPCBinClient(conn)
-	fmt.Println(client)
+	ctx := context.Background()
+
+	// call DummyUnary
+	res, err := client.DummyUnary(ctx, &pb.DummyMessage{
+		FString: "hello",
+		FInt32:  42,
+	})
+	if err != nil {
+		log.Fatalf("failed to call DummyUnary: %v", err)
+	}
+	fmt.Println(res)
 }
