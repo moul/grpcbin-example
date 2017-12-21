ed0f575246de662e5881bae9bcf51e1a5a8d7a1c
Manfred Touron <m@42.am>
2017-12-21 23:53:34 +0100

    Use TLS port

--- go/grpcbin-example-go/main.go
+++ go/grpcbin-example-go/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	pb "github.com/moul/pb/grpcbin/go-grpc"
 )
 
 func main() {
 	// dial
-	conn, _ := grpc.Dial("grpcb.in:9000", grpc.WithInsecure())
+	// conn, _ := grpc.Dial("grpcb.in:9000", grpc.WithInsecure())
+	conn, _ := grpc.Dial("grpcb.in:9001", grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{ServerName: "", InsecureSkipVerify: true})))
 	defer conn.Close()
 
 	// create client and context
